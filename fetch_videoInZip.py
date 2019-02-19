68331d761b361e899c92692c3d0941493e2453fc
chiudk <weixiaosmile010203@outlook.com>
2019-02-19 18:20:55 +0800

    测试

---
+++ fetch_videoInZip.py
@@ -0,0 +1,32 @@
+import os
+import zipfile
+import sys
+
+
+def unzip(zip_path,unzip_path):
+    try:
+        f = zipfile.ZipFile(zip_path,'r')
+        for file in f.namelist():
+            file_suffix = os.path.splitext(file)[-1]
+            if file_suffix == '.mp4' or file_suffix == '.mkv' or file_suffix == '.avi':
+                f.extract(file,unzip_path)
+        f.close
+    except:
+        print('Error file not is ziptype')
+
+
+
+def zip_list(files_path):
+    file_list = []
+    for root, folder, files in os.walk('./'):
+        for file in files:
+            file_list.append(os.path.join(root,file))
+    return file_list
+
+zipfiles = zip_list('./')
+for zip_path in zipfiles:
+    print(zip_path)
+    unzip(zip_path, './')
+
+
+
