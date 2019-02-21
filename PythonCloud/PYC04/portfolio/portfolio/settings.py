f264e2e21a270a8759d12d8eb1e18f24ba123dce
chiudk <weixiaosmile010203@outlook.com>
2019-02-21 18:19:23 +0800

    添加django的静态文件  迁移静态文件

--- PythonCloud/PYC04/portfolio/portfolio/settings.py
+++ PythonCloud/PYC04/portfolio/portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'q(4%9g!lposj3e8a^sdtl2*)x&h$@rfo9g*x_qrhkn#rb57m77'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,7 +118,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'portfolio/static/'),
+]
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
