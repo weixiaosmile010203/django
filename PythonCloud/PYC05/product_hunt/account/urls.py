b1da63b6a0c52e907f44de1ee59399e2fa1b28b8
chiudk <weixiaosmile010203@outlook.com>
2019-02-27 01:02:33 +0800

    增加用户登录和用户退出

--- PythonCloud/PYC05/product_hunt/account/urls.py
+++ PythonCloud/PYC05/product_hunt/account/urls.py
@@ -17,4 +17,6 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
+    path('signin/', views.signin, name='signin'),
+    path('signout/', views.signout, name='signout'),
 ]
