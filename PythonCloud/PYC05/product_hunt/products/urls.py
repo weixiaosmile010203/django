e8c615c4ca86d97a0dbdd465094c5414ad2f1184
chiudk <weixiaosmile010203@outlook.com>
2019-02-28 01:12:07 +0800

    添加发布产品的功能(未完善)

--- PythonCloud/PYC05/product_hunt/products/urls.py
+++ PythonCloud/PYC05/product_hunt/products/urls.py
@@ -13,12 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
-import products.views
+from . import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', products.views.products, name='首页'),
-    path('account/',include('account.urls')),
-    path('products/',include('products.urls'))
+    path('publish', views.publish, name='release'),
 ]
--- PythonCloud/PYC05/product_hunt/products/views.py
+++ PythonCloud/PYC05/product_hunt/products/views.py
@@ -7,3 +7,15 @@ from django.shortcuts import render
 
 def products(request):
     return render(request, 'products.html')
+
+
+def publish(request):
+    if request.method == 'GET':
+        return render(request, 'publist.html')
+    elif request.method == 'POST':
+        app_name = request.POST["APP名称"]
+        intrp = request.POST["介绍"]
+        url = request.POST["APP链接"]
+        icon = request.FILES["APP图标"]
+        image = request.FILES["大图"]
+        return render(request, 'publish.html')
